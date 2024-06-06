d73568431ce49eab4de66ca31efb2a909fa393bb
GunnerBel <155954642+GunnerBel@users.noreply.github.com>
2024-06-06 13:43:58 +0000

    final

---
+++ gallery.js
@@ -0,0 +1,133 @@
+
+
+const image = document.getElementById("zoom");
+
+//event listener for the mouseover
+
+image.addEventListener('mouseover', function() {
+
+    image.style.transform = "scale(1.1)";
+    image.style.transition = "transform o.5s ease";
+});
+
+image.addEventListener('mouseout', function() {
+
+    image.style.transform = 'scale(1)';
+});
+
+
+
+const imagee = document.getElementById("zoom2");
+
+//event listener for the mouseover
+
+imagee.addEventListener('mouseover', function() {
+
+    imagee.style.transform = "scale(1.1)";
+    imagee.style.transition = "transform o.5s ease";
+});
+
+imagee.addEventListener('mouseout', function() {
+
+    imagee.style.transform = 'scale(1)';
+});
+
+
+const image3 = document.getElementById("zoom3");
+
+//event listener for the mouseover
+
+image3.addEventListener('mouseover', function() {
+
+    image3.style.transform = "scale(1.1)";
+    image3.style.transition = "transform o.5s ease";
+});
+
+image3.addEventListener('mouseout', function() {
+
+    image3.style.transform = 'scale(1)';
+});
+
+
+const image4 = document.getElementById("zoom4");
+
+//event listener for the mouseover
+
+image4.addEventListener('mouseover', function() {
+
+    image4.style.transform = "scale(1.1)";
+    image4.style.transition = "transform o.5s ease";
+});
+
+image4.addEventListener('mouseout', function() {
+
+    image4.style.transform = 'scale(1)';
+});
+
+
+
+
+const imae = document.getElementById("zom");
+
+//event listener for the mouseover
+
+imae.addEventListener('mouseover', function() {
+
+    imae.style.transform = "scale(1.1)";
+    imae.style.transition = "transform o.5s ease";
+});
+
+imae.addEventListener('mouseout', function() {
+
+    imae.style.transform = 'scale(1)';
+});
+
+
+
+const imageee = document.getElementById("zom2");
+
+//event listener for the mouseover
+
+imageee.addEventListener('mouseover', function() {
+
+    imageee.style.transform = "scale(1.1)";
+    imageee.style.transition = "transform o.5s ease";
+});
+
+imageee.addEventListener('mouseout', function() {
+
+    imageee.style.transform = 'scale(1)';
+});
+
+
+const image33 = document.getElementById("zom3");
+
+//event listener for the mouseover
+
+image33.addEventListener('mouseover', function() {
+
+    image33.style.transform = "scale(1.1)";
+    image33.style.transition = "transform o.5s ease";
+});
+
+image33.addEventListener('mouseout', function() {
+
+    image33.style.transform = 'scale(1)';
+});
+
+
+const image44 = document.getElementById("zom4");
+
+//event listener for the mouseover
+
+image44.addEventListener('mouseover', function() {
+
+    image44.style.transform = "scale(1.1)";
+    image44.style.transition = "transform o.5s ease";
+});
+
+image44.addEventListener('mouseout', function() {
+
+    image44.style.transform = 'scale(1)';
+});
+
